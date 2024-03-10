02b2d372722cb5992d7a9183259fed1d0bc2c53b
Kawther-07 <kawther.rached@univ-constantine2.dz>
2024-03-10 20:35:54 +0100

    register is  working, login is not

---
+++ lib/api_service.dart
@@ -0,0 +1,22 @@
+// import 'package:http/http.dart' as http;
+
+// class ApiService {
+//   static const String baseUrl = 'http://localhost:3000/api';
+
+
+
+
+
+
+//   static Future<http.Response> login(String email, String password) async {
+//     final response = await http.post(
+//       Uri.parse('$baseUrl/login'),
+//       body: {
+//         'email': email,
+//         'password': password,
+//       },
+//     );
+//     return response;
+//   }
+
+// }
