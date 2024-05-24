8107ecae238b50eadc57781920c87e501395f4c6
Kawther-07 <kawther.rached@univ-constantine2.dz>
2024-05-24 15:53:00 +0200

    started working with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,15 @@ import 'package:DoolabMobile/pages/register_page.dart';
 import 'package:DoolabMobile/pages/stats.dart';
 import 'package:DoolabMobile/pages/home_page.dart';
 import 'package:DoolabMobile/pages/patient_profile_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Ensures Flutter binding is initialized
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ); // Initialize Firebase
+  runApp(MyApp()); // Run the app
 }
 
 class MyApp extends StatelessWidget {
